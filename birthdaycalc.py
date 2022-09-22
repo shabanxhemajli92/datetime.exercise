189cde6daf8f8b1fccd73ac8579efa59b06f32c9
Shaban <shabanxhemajli@live.com>
2022-09-22 13:29:22 +0200

    final version

--- birthdaycalc.py
+++ birthdaycalc.py
@@ -6,11 +6,11 @@ bday_input=input("Type in your birthday: ")
 some_objct = datetime.datetime.fromisoformat(bday_input)
 todays_date=datetime.datetime.now()
 age_years=(todays_date-some_objct)
-print(age_years)
-days=int(input("Enter the days Day:"))
+
+days=age_years.days
 
 years = days // 365
 weeks = days // 7
 months = days // 30
 
-print("Your duration in the world so far is:",years,"years",weeks,"weeks","and",months,"months since you came here.")
+print("Your duration in the world so far is:",years,"years","or",weeks,"weeks","or",months,"months since your mother validated your existance")
--- eventplanner.py
+++ eventplanner.py
@@ -7,7 +7,7 @@ some_objct = datetime.datetime.fromisoformat(event_date)
 todays_date=datetime.datetime.now()
 date_of_event=(some_objct-todays_date)
 print(date_of_event)
-days=int(input("Enter the days Day:"))
+days=date_of_event.days
 
 years = days // 365
 weeks = (days - (years* 365)) // 7
